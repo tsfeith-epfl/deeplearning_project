effde4c014d36326779f56c835330a60d08155b8
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2022-04-11 15:58:08 +0200

    minor update

--- Miniproject_1/model.py
+++ Miniproject_1/model.py
@@ -19,6 +19,7 @@ class Model ():
         self.nb_epochs = 250
         self.mini_batch_size = 100 
         
+        # don't use this! Instead mimic what they used in the paper, then if we have time we can work from there
         self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
         self.conv2 = nn.Conv2d(32, 32, kernel_size=5)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=2)
