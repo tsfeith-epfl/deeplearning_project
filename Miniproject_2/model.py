2a5fe06ee6c64c55ee3c76952e8921c4ecfe62e4
fraborg99 <francesco_borg@yahoo.it>
2022-04-26 18:15:36 +0200

    activations and loss

--- Miniproject_2/model.py
+++ Miniproject_2/model.py
@@ -1,3 +1,7 @@
+import math
+import torch
+torch.set_grad_enabled(False)
+
 # we want to build the following model
 # Sequential (Conv(stride 2),
 #             ReLU,
@@ -9,6 +13,7 @@
 #             Sigmoid)
 
 
+# +
 # Suggested structure
 # HOWEVER GRADING WILL REWARD ORIGINALITY
 class Module(object):
@@ -18,6 +23,68 @@ class Module(object):
         raise NotImplementedError
     def param(self):
         return []
+    
+## ACTIVATION FUNCTIONS
+    
+class ReLU(Module): 
+    def forward(self, input_):
+        """
+        ReLU(x) = max(0, x): returns the max between 0 and the input
+        """
+        return max(0, input_)
+    
+    def backward(self, input_):
+        """
+        Derivative of ReLU: 1 if input > 0, 0 elsewhere
+        """
+        return int(input_>0)
+    
+class Sigmoid(Module):
+    def forward(self, input_):
+        """
+        Sigmoid(x) = 1/(1+e^(-x))
+        """
+        return 1/(1+math.exp(-input_))
+    
+    def backward(self, input_):
+        """
+        Derivative of sigmoid: dsig(x)/dx = sig(x)(1-sig(x))
+        """
+        return forward(input_)*(1-forward(input_))
+    
+## LOSS FUNCTIONS
+
+class MSE(Module):
+    def __init__(self, predictions, targets):
+        self.size = len(predictions)
+        self.predictions, self.targets = predictions, targets
+    
+    def forward(self):
+        """
+        Mean Squared Error: MSE(x) = 1/N * (y - f(x))^2
+        """
+        return sum([(self.targets[i] - self.predictions[i])**2 for i in range(self.size)]) / self.size
+    
+    def backward(self):
+        """
+        Derivative of MSE = -2/N * (y - f(x))
+        """
+        return -2*sum([(self.targets[i] - self.predictions[i]) for i in range(self.size)]) / self.size
+        
+    
+
+
+# -
+
+class SGD():
+    def __init__(self, params, lr):
+        self.params = params
+        self.lr = lr
+    
+    def step(self):
+        for p in self.params: p -= lr*grad?
+        
+
 
 # We need to define the following modules: Conv2d, TransposeConv2d or
 # NearestUpsampling, ReLU, Sigmoid, MSE, SGD, Sequential
@@ -80,3 +147,26 @@ class Model ():
         """
 
         pass
+
+
+class MSE(object):
+    def forward(self, *args):
+        raise NotImplementedError
+    def backward(self, *gradwrtoutput):
+        raise NotImplementedError
+    def param(self):
+        return []
+
+
+    
+    def mse(self, prediction, target):
+        """
+        Perform mean squared error given targets and predictions.
+        """
+        return torch.sum((prediction - target)**2)/prediction 
+    
+    def relu(self, activation):
+        return max(0, activation)
+    
+    def sigmoid(self, activation):
+        return 1/(1+exp(-activation))
